f780e76b13aefceb7a54c248fb88ebff3c265adc
Nick Blaisdell <nick.blaisdell@gmail.com>
2016-10-20 15:40:34 -0400

    pages up and linked starting to work on TDD

--- app.rb
+++ app.rb
@@ -6,16 +6,15 @@ require_relative "models/team_data.rb"
 set :bind, '0.0.0.0'  # bind to all interfaces
 
 get "/LACKP" do
-  "<h1>The LACKP Homepage<h1>"
   erb :lackp
 end
 
-get "/teams" do
+get "/LACKP/teams" do
   @teams = ["Simpson Slammers", "Jetson Jets", "Flintstone Fire", "Griffin Goats"]
   erb :teams
 end
 
-get "/LACKP/:team_name" do
+get "/LACKP/teams/:team_name" do
   @team_name = params[:team_name]
   erb :team_page
 end
